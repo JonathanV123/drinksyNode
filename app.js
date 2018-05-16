456b56671fcb888af81e40d147c16d381f511140
Jonathan <jvoxland@gmail.com>
2018-05-16 15:06:31 -0700

    enabled CORS for development to grab data from react front-end

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
+const cors = require('cors');
 const pg = require('pg');
 
 require('dotenv').config({path: 'variables.env'})
@@ -8,10 +9,21 @@ const app = express();
 
 const connectionString = process.env.DATABASE;
 
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', "*");
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+}
+
+app.use(cors());
+
 app.get('/', (req, res) => {
+    const testing = 'Hi!';
+    res.send({express: 'Hello From Express'});
     console.log(process.env.DATABASE)
 });
 
-app.listen(3000, () => {
-    console.log("Server Started On Port 3000")
+app.listen(8080, () => {
+    console.log("Server Started On Port 8080")
 });
