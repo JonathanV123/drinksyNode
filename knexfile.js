0d355c74d16f7f6f6ce9971d455302614938ff19
Jonathan <jvoxland@gmail.com>
2018-06-18 18:16:18 -0700

    basic test structure

--- knexfile.js
+++ knexfile.js
@@ -6,22 +6,10 @@ module.exports = {
     client: 'pg',
     connection: connectionString
   },
-
-  // staging: {
-  //   client: 'postgresql',
-  //   connection: {
-  //     database: 'my_db',
-  //     user:     'username',
-  //     password: 'password'
-  //   },
-  //   pool: {
-  //     min: 2,
-  //     max: 10
-  //   },
-  //   migrations: {
-  //     tableName: 'knex_migrations'
-  //   }
-  // },
+  test:{
+    client: 'pg',
+    connection: 'postgres://postgres:postgres@127.0.0.1:5432/test-restaurants'
+  }
 
   // production: {
   //   client: 'postgresql',
