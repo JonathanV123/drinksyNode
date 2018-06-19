b0552d59f54bcd65d31779efa2f4042e9c70e1d0
Jonathan <jvoxland@gmail.com>
2018-06-19 15:58:17 -0700

    added middleware to test for a valid ID

--- controllers/restaurantsController.js
+++ controllers/restaurantsController.js
@@ -4,3 +4,8 @@ exports.get_all_restaurants = async (req, res) => {
     const all_restaurants = await queries.getAll();
         res.json(all_restaurants);
 };
+
+exports.get_restaurant_by_id = async (req, res) => {
+    // const all_restaurants = await queries.getAll();
+        res.json('Hi!');
+};
--- routes/routingIndex.js
+++ routes/routingIndex.js
@@ -1,7 +1,18 @@
 const express = require('express');
 const router = express.Router();
+const { catchErrors } = require('../errorHandler/errorHandling');
+const { isValidId } = require('../middleware/validation');
 const restaurant_controller = require('../controllers/restaurantsController');
 
-router.get('/', restaurant_controller.get_all_restaurants)
+
+
+router.get('/', catchErrors(restaurant_controller.get_all_restaurants)
+)
+
+
+router.get('/:id',
+    isValidId,
+    catchErrors(restaurant_controller.get_restaurant_by_id)
+);
 
 module.exports = router;
