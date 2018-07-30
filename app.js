9815c9b00d2b1b208da8a46f3977a09f5263c269
Jonathan <jvoxland@gmail.com>
2018-07-30 13:34:31 -0700

    cors fix attempt #4

--- app.js
+++ app.js
@@ -36,11 +36,17 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 // app.use(cors());
 
-app.use(function(req, res, next) {
+app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  if (req.method === "OPTIONS") {
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+    return res.status(200).json({});
+  }
   next();
 });
+
 app.use('/', routes);
 
 // app.use('/api/v1/restaurants', routes);
