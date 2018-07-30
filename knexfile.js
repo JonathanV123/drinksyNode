98358f7ff7bc8e71aaf331941c4b45972dbaeae7
Jonathan <jvoxland@gmail.com>
2018-07-30 11:44:27 -0700

    added DB connection to heroku

--- knexfile.js
+++ knexfile.js
@@ -21,22 +21,27 @@ module.exports = {
     seeds: {
       directory: __dirname + '/db/seeds/testSeeds'
     }
+  },
+  production: {
+    client: 'pg',
+    connection: {
+      port: process.env.DATABASE_PORT,
+      host: process.env.DATABASE_HOST,
+      database: process.env.DATABASE_NAME,
+      user: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      ssl: true,
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      directory: __dirname + '/db/migrations'
+    },
+    seeds: {
+      directory: __dirname + '/db/seeds'
+    }
   }
-
-  // production: {
-  //   client: 'postgresql',
-  //   connection: {
-  //     database: 'my_db',
-  //     user:     'username',
-  //     password: 'password'
-  //   },
-  //   pool: {
-  //     min: 2,
-  //     max: 10
-  //   },
-  //   migrations: {
-  //     tableName: 'knex_migrations'
-  //   }
-  // }
 
 };
