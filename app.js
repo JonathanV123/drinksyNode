d5af362f440a5eaf507fb24e738a87ee505b69e0
Jonathan <jvoxland@gmail.com>
2018-07-31 15:51:03 -0700

    cors refactor

--- app.js
+++ app.js
@@ -34,7 +34,6 @@ app.use(bodyParser.json());
 app.use(morgan('tiny'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(cors());
 
 // app.use((req, res, next) => {
 //   res.header("Access-Control-Allow-Origin", "*");
